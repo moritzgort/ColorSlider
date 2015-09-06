d85372c487d926217d02759734267aefc16d2c6d
Moritz <revil4.artwork@gmail.com>
2015-09-06 09:06:52 +0200

    Start Basic functions

--- ColorSlider/ViewController.swift
+++ ColorSlider/ViewController.swift
@@ -10,14 +10,26 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var colorView: UIView!
+    
+    @IBOutlet weak var redSlideValue: UISlider!
+    @IBOutlet weak var greenSlideValue: UISlider!
+    @IBOutlet weak var blueSlideValue: UISlider!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func slideChanged(sender: UISlider) {
+        updateColor()
+    }
+    
+    func updateColor() {
+        colorView.backgroundColor = UIColor(red: CGFloat(redSlideValue.value), green: CGFloat(greenSlideValue.value), blue: CGFloat(blueSlideValue.value), alpha: 1.0)
     }
 
 
